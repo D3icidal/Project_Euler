f31ad87b15add91f000a9ed868cc60ae68f0b7d7
Thomas Ng <D3icidal@gmail.com>
2018-10-19 16:26:31 -0400

    adds solution for problem 10

--- 10.rb
+++ 10.rb
@@ -3,3 +3,29 @@
 # Find the sum of all the primes below two million.
 
 
+mtable = [] #nil = prime (because it does not show up on multiplication table)
+
+array_of_primes = []
+target = 2000000 #all primes below this number
+
+sum_of_primes = 0
+
+half = target/2
+stop = Math.sqrt(target) - 1
+puts "stop is at #{stop}"
+
+(2..stop).each do |x|
+  (2..half).each do |y|
+    break if x*y > target
+    # puts "x: #{x} * y:#{y} = #{x*y}"
+    mtable[x*y] = true
+  end
+end
+
+mtable[0] = true
+mtable[1] = true
+p mtable
+mtable.each_index{ |i|  array_of_primes << i if mtable[i] == nil }
+p array_of_primes
+# array_of_primes.reject! {|x| x > 3}
+puts array_of_primes.reduce(0, :+)
